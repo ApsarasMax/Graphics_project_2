8d76c27bd9675f40e81c1671fb463f048c64c669
ApsarasMax <yichizhang810@gmail.com>
2016-02-28 19:28:35 -0600

    Added files via upload
    kdtree built but wrong

---
+++ ray_tree_built/src/RayTracer.cpp
@@ -0,0 +1,234 @@
+// The main ray tracer.
+
+#pragma warning (disable: 4786)
+
+#include "RayTracer.h"
+#include "scene/light.h"
+#include "scene/material.h"
+#include "scene/ray.h"
+
+#include "parser/Tokenizer.h"
+#include "parser/Parser.h"
+
+//#include "ui/TraceUI.h"
+#include "ui/GraphicalUI.h"//zyc
+#include <cmath>
+#include <algorithm>
+
+extern TraceUI* traceUI;
+extern GraphicalUI* graphicalUI;//zyc
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Use this variable to decide if you want to print out
+// debugging messages.  Gets set in the "trace single ray" mode
+// in TraceGLWindow, for example.
+bool debugMode = false;
+
+// Trace a top-level ray through pixel(i,j), i.e. normalized window coordinates (x,y),
+// through the projection plane, and out into the scene.  All we do is
+// enter the main ray-tracing method, getting things started by plugging
+// in an initial ray weight of (0.0,0.0,0.0) and an initial recursion depth of 0.
+
+Vec3d RayTracer::trace(double x, double y)
+{
+  // Clear out the ray cache in the scene for debugging purposes,
+  if (TraceUI::m_debug) scene->intersectCache.clear();
+  ray r(Vec3d(0,0,0), Vec3d(0,0,0), ray::VISIBILITY);
+  scene->getCamera().rayThrough(x,y,r);
+  Vec3d ret = traceRay(r, traceUI->getDepth());
+  ret.clamp();
+  return ret;
+}
+
+Vec3d RayTracer::tracePixel(int i, int j)
+{
+	Vec3d col(0,0,0);
+
+	if( ! sceneLoaded() ) return col;
+
+	double x = double(i)/double(buffer_width);
+	double y = double(j)/double(buffer_height);
+
+	unsigned char *pixel = buffer + ( i + j * buffer_width ) * 3;
+
+	col = trace(x, y);
+
+	pixel[0] = (int)( 255.0 * col[0]);
+	pixel[1] = (int)( 255.0 * col[1]);
+	pixel[2] = (int)( 255.0 * col[2]);
+	return col;
+}
+
+
+// Do recursive ray tracing!  You'll want to insert a lot of code here
+// (or places called from here) to handle reflection, refraction, etc etc.
+Vec3d RayTracer::traceRay(ray& r, int depth)
+{
+	isect i;
+	Vec3d colorC;
+
+	if(scene->intersect(r, i)) {
+		// YOUR CODE HERE
+
+		// An intersection occurred!  We've got work to do.  For now,
+		// this code gets the material for the surface that was intersected,
+		// and asks that material to provide a color for the ray.  
+
+		// This is a great place to insert code for recursive ray tracing.
+		// Instead of just returning the result of shade(), add some
+		// more steps: add in the contributions from reflected and refracted
+		// rays.
+		const Material& m = i.getMaterial();
+	  	colorC = m.shade(scene, r, i);
+
+	  	if(depth <= 0){
+	  		return colorC;
+	  	}
+
+	  	//reflection
+	  	Vec3d N = i.N;
+	    Vec3d L = -r.d; //opposite of ray direction
+	    L.normalize();
+
+	    //if(!(m.kr(i)[0]==0 && m.kr(i)[1]==0 && m.kr(i)[2]==0)){
+	    Vec3d dirReflect = 2*max((L*N), 0.0)*N-L;
+	    dirReflect.normalize();
+
+	    ray reflect(r.at(i.t), dirReflect, ray :: REFLECTION);
+
+	    colorC += m.kr(i) % traceRay(reflect, depth-1);
+		//}
+
+		//refraction
+		double yita_i=1.0;// = m.index(i); //object = i
+	    double yita_t=1.0;// = 1.0; //air =t
+	    double yita=1.0;// = yita_i/yita_t;
+		double cosine_i = N * L;
+		double flag = 1;
+	    if(cosine_i>0){
+	    	yita_i = 1.0; //air = i
+		    yita_t = m.index(i); //object =t
+		    yita = yita_i/yita_t;
+		    
+	    }else{
+	    	yita_i = m.index(i); //object = i
+		    yita_t = 1.0; //air =t
+		    yita = yita_i/yita_t;
+		    colorC -= m.shade(scene, r, i);
+		    
+		    flag = -1;
+
+		    double sine_i = sqrt(1 - cosine_i*cosine_i);
+		    double sine_t = yita * sine_i;
+		    if(sine_t>1){
+		    	return colorC;
+		    }
+	    }
+
+	    double cosine_t = sqrt(1-yita*yita*(1-cosine_i*cosine_i));
+
+	    Vec3d dirRefract = (yita*cosine_i - cosine_t * flag) * N  - yita * L;
+	    dirRefract.normalize();
+	    ray refract(r.at(i.t), dirRefract, ray :: REFRACTION);
+	    colorC += m.kt(i) % traceRay(refract, depth-1);
+
+	  
+	} else {
+		// No intersection.  This ray travels to infinity, so we color
+		// it according to the background color, which in this (simple) case
+		// is just black.
+		colorC = Vec3d(0.0, 0.0, 0.0);
+	}
+	return colorC;
+}
+
+RayTracer::RayTracer()
+	: scene(0), buffer(0), buffer_width(256), buffer_height(256), m_bBufferReady(false)
+{}
+
+RayTracer::~RayTracer()
+{
+	delete scene;
+	delete [] buffer;
+}
+
+void RayTracer::getBuffer( unsigned char *&buf, int &w, int &h )
+{
+	buf = buffer;
+	w = buffer_width;
+	h = buffer_height;
+}
+
+double RayTracer::aspectRatio()
+{
+	return sceneLoaded() ? scene->getCamera().getAspectRatio() : 1;
+}
+
+bool RayTracer::loadScene( char* fn ) {
+	ifstream ifs( fn );
+	if( !ifs ) {
+		string msg( "Error: couldn't read scene file " );
+		msg.append( fn );
+		traceUI->alert( msg );
+		return false;
+	}
+	
+	// Strip off filename, leaving only the path:
+	string path( fn );
+	if( path.find_last_of( "\\/" ) == string::npos ) path = ".";
+	else path = path.substr(0, path.find_last_of( "\\/" ));
+
+	// Call this with 'true' for debug output from the tokenizer
+	Tokenizer tokenizer( ifs, false );
+    Parser parser( tokenizer, path );
+	try {
+		delete scene;
+		scene = 0;
+		scene = parser.parseScene();
+	} 
+	catch( SyntaxErrorException& pe ) {
+		traceUI->alert( pe.formattedMessage() );
+		return false;
+	}
+	catch( ParserException& pe ) {
+		string msg( "Parser: fatal exception " );
+		msg.append( pe.message() );
+		traceUI->alert( msg );
+		return false;
+	}
+	catch( TextureMapException e ) {
+		string msg( "Texture mapping exception: " );
+		msg.append( e.message() );
+		traceUI->alert( msg );
+		return false;
+	}
+
+	if( !sceneLoaded() ) return false;
+
+	if(graphicalUI->m_kdtreeInfo){//cindy
+		scene->buildKdTree(graphicalUI->m_nKdtreeMaxDepth, graphicalUI->m_nKdtreeLeafSize);
+		//cout<<graphicalUI->m_nKdtreeLeafSize<<endl;
+		//cout<<graphicalUI->m_kdtreeInfo<<endl;
+	}
+
+	return true;
+}
+
+void RayTracer::traceSetup(int w, int h)
+{
+	if (buffer_width != w || buffer_height != h)
+	{
+		buffer_width = w;
+		buffer_height = h;
+		bufferSize = buffer_width * buffer_height * 3;
+		delete[] buffer;
+		buffer = new unsigned char[bufferSize];
+	}
+	memset(buffer, 0, w*h*3);
+	m_bBufferReady = true;
+}
+
---
+++ ray_tree_built/src/scene/scene.h
@@ -0,0 +1,524 @@
+//
+// scene.h
+//
+// The Scene class and the geometric types that it can contain.
+//
+
+#pragma warning (disable: 4786)
+
+
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <memory>
+#include <stack>//cindy
+#include <assert.h>//cindy
+
+#include "ray.h"
+#include "material.h"
+#include "camera.h"
+#include "bbox.h"
+
+#include "../vecmath/vec.h"
+#include "../vecmath/mat.h"
+
+using namespace std;
+
+class Light;
+class Scene;
+
+
+
+class SceneElement {
+
+public:
+  virtual ~SceneElement() {}
+
+  Scene *getScene() const { return scene; }
+
+  // For debugging purposes, draws using OpenGL
+  virtual void glDraw(int quality, bool actualMaterials, bool actualTextures) const  { }
+
+ protected:
+ SceneElement( Scene *s )
+   : scene( s ) {}
+
+  Scene *scene;
+};
+
+class TransformNode {
+
+protected:
+
+  // information about this node's transformation
+  Mat4d    xform;
+  Mat4d    inverse;
+  Mat3d    normi;
+
+  // information about parent & children
+  TransformNode *parent;
+  std::vector<TransformNode*> children;
+    
+ public:
+  typedef std::vector<TransformNode*>::iterator          child_iter;
+  typedef std::vector<TransformNode*>::const_iterator    child_citer;
+
+  ~TransformNode() {
+      for(child_iter c = children.begin(); c != children.end(); ++c ) delete (*c);
+    }
+
+  TransformNode *createChild(const Mat4d& xform) {
+    TransformNode *child = new TransformNode(this, xform);
+    children.push_back(child);
+    return child;
+  }
+    
+  // Coordinate-Space transformation
+  Vec3d globalToLocalCoords(const Vec3d &v) { return inverse * v; }
+
+  Vec3d localToGlobalCoords(const Vec3d &v) { return xform * v; }
+
+  Vec4d localToGlobalCoords(const Vec4d &v) { return xform * v; }
+
+  Vec3d localToGlobalCoordsNormal(const Vec3d &v) {
+    Vec3d ret = normi * v;
+    ret.normalize();
+    return ret;
+  }
+
+  const Mat4d& transform() const		{ return xform; }
+
+protected:
+  // protected so that users can't directly construct one of these...
+  // force them to use the createChild() method.  Note that they CAN
+  // directly create a TransformRoot object.
+ TransformNode(TransformNode *parent, const Mat4d& xform ) : children() {
+      this->parent = parent;
+      if (parent == NULL) this->xform = xform;
+      else this->xform = parent->xform * xform;  
+      inverse = this->xform.inverse();
+      normi = this->xform.upper33().inverse().transpose();
+    }
+};
+
+class TransformRoot : public TransformNode {
+ public:
+ TransformRoot() : TransformNode(NULL, Mat4d()) {}
+};
+
+// A Geometry object is anything that has extent in three dimensions.
+// It may not be an actual visible scene object.  For example, hierarchical
+// spatial subdivision could be expressed in terms of Geometry instances.
+class Geometry : public SceneElement {
+
+protected:
+  // intersections performed in the object's local coordinate space
+  // do not call directly - this should only be called by intersect()
+  virtual bool intersectLocal(ray& r, isect& i ) const = 0;
+
+public:
+  // intersections performed in the global coordinate space.
+  bool intersect(ray& r, isect& i) const;
+
+  virtual bool hasBoundingBoxCapability() const;
+  const BoundingBox& getBoundingBox() const { return bounds; }
+  Vec3d getNormal() { return Vec3d(1.0, 0.0, 0.0); }
+
+  virtual void ComputeBoundingBox() {
+    // take the object's local bounding box, transform all 8 points on it,
+    // and use those to find a new bounding box.
+
+    BoundingBox localBounds = ComputeLocalBoundingBox();
+        
+    Vec3d min = localBounds.getMin();
+    Vec3d max = localBounds.getMax();
+
+    Vec4d v, newMax, newMin;
+
+    v = transform->localToGlobalCoords( Vec4d(min[0], min[1], min[2], 1) );
+    newMax = v;
+    newMin = v;
+    v = transform->localToGlobalCoords( Vec4d(max[0], min[1], min[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+    v = transform->localToGlobalCoords( Vec4d(min[0], max[1], min[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+    v = transform->localToGlobalCoords( Vec4d(max[0], max[1], min[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+    v = transform->localToGlobalCoords( Vec4d(min[0], min[1], max[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+    v = transform->localToGlobalCoords( Vec4d(max[0], min[1], max[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+    v = transform->localToGlobalCoords( Vec4d(min[0], max[1], max[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+    v = transform->localToGlobalCoords( Vec4d(max[0], max[1], max[2], 1) );
+    newMax = maximum(newMax, v);
+    newMin = minimum(newMin, v);
+		
+    bounds.setMax(Vec3d(newMax));
+    bounds.setMin(Vec3d(newMin));
+  }
+
+  // default method for ComputeLocalBoundingBox returns a bogus bounding box;
+  // this should be overridden if hasBoundingBoxCapability() is true.
+  virtual BoundingBox ComputeLocalBoundingBox() { return BoundingBox(); }
+
+  void setTransform(TransformNode *transform) { this->transform = transform; };
+    
+ Geometry(Scene *scene) : SceneElement( scene ) {}
+
+  // For debugging purposes, draws using OpenGL
+  void glDraw(int quality, bool actualMaterials, bool actualTextures) const;
+
+  // The defult does nothing; this is here because it is not required
+  // that you implement this function if you create your own scene objects.
+  virtual void glDrawLocal(int quality, bool actualMaterials, bool actualTextures) const { }
+  virtual void buildKdTree() {}//cindy//mark
+
+  
+ protected:
+  BoundingBox bounds;
+  TransformNode *transform;
+};
+
+// A SceneObject is a real actual thing that we want to model in the 
+// world.  It has extent (its Geometry heritage) and surface properties
+// (its material binding).  The decision of how to store that material
+// is left up to the subclass.
+class SceneObject : public Geometry {
+
+ public:
+  virtual const Material& getMaterial() const = 0;
+  virtual void setMaterial(Material *m) = 0;
+
+  void glDraw(int quality, bool actualMaterials, bool actualTextures) const;
+
+ protected:
+ SceneObject( Scene *scene )
+   : Geometry( scene ) {}
+};
+
+// A simple extension of SceneObject that adds an instance of Material
+// for simple material bindings.
+class MaterialSceneObject : public SceneObject {
+
+public:
+  virtual ~MaterialSceneObject() { delete material; }
+
+  virtual const Material& getMaterial() const { return *material; }
+  virtual void setMaterial(Material* m)	{ delete material; material = m; }
+
+protected:
+ MaterialSceneObject(Scene *scene, Material *mat) 
+   : SceneObject(scene), material(mat) {}
+
+  Material* material;
+};
+
+//cindy
+ // template <typename Obj>//cindy
+  //class KdTree;
+class KdTree{
+    private:
+        int currentAxis;
+        int depth;
+        BoundingBox bbox;
+        std::vector <Geometry*> objects;
+        double split;
+        int size;
+        
+    public:
+        KdTree* left;
+        KdTree* right;
+        KdTree (int depth, BoundingBox bbox, int size) {
+          this->bbox = bbox;
+          this->depth = depth;
+          currentAxis = 0;
+          this->size = size;
+          left = nullptr;
+          right = nullptr;
+        }
+
+        // KdTree (BoundingBox b) {
+        //   left = nullptr;
+        //   right = nullptr;
+        //   boundary = b;
+        // }
+        ~KdTree() {
+          if (!objects.empty())
+            objects.clear();
+          if (left != nullptr)
+            delete left;
+          if (right != nullptr)
+            delete right;
+        }
+        std::vector<Geometry*> getObjects() {
+          return objects;
+        }
+
+        // BoundingBox getBoundary() {
+        //   return bbox;
+        // }
+
+        void setCurrentAxis(int currentAxis) {
+          this->currentAxis = currentAxis;
+        }
+
+        int getCurrentAxis() {
+          return currentAxis;
+        }
+
+
+        void addObjects (std::vector<Geometry*> &objects) {
+            if (objects.size() < size || depth <= 0) {
+              this->objects = objects;
+              return;
+            }
+            Vec3d min = bbox.getMin();
+            Vec3d max = bbox.getMax();
+            split = (min[currentAxis] + max[currentAxis]) / 2.0;
+            std::vector<Geometry*> vleft;
+            std::vector<Geometry*> vright;
+            bool added = false;
+            int countLeft = 0;
+            for(auto obj: objects){
+                added = false;
+                if (!obj->hasBoundingBoxCapability())
+                  continue;
+                if (obj->getBoundingBox().getMin()[currentAxis] <= split) {
+                    added = true;
+                    vleft.push_back(obj);
+                    countLeft ++;
+                }
+                if (obj->getBoundingBox().getMax()[currentAxis] >= split) {
+                    added = true;
+                    vright.push_back(obj);
+                }
+                // debug: if everything is added
+                assert(added);
+
+            }
+
+            // trim tree leaves
+            if (countLeft == 0 || countLeft == objects.size()) {
+              this->objects = objects;
+              return;
+            }
+            
+            // sort geometry
+
+            Vec3d middle0 = min;
+            middle0[currentAxis] = split;
+            Vec3d middle1 = max;
+            middle1[currentAxis] = split;
+            
+            // recursively build tree
+            left = new KdTree(depth - 1, BoundingBox(min, middle1), size);
+            left->setCurrentAxis((currentAxis + 1) % 3);
+            left->addObjects(vleft);
+            right = new KdTree(depth - 1, BoundingBox(middle0, max), size);
+            right->setCurrentAxis((currentAxis + 1) % 3);
+            right->addObjects(vright);
+      }
+
+
+    bool intersect(ray& r, isect& i) {
+        double tmin = 0.0;
+        double tmax = 0.0;
+        // get intersection time
+        bool have_one = bbox.intersect(r, tmin, tmax);
+        if (!have_one) 
+          return false;
+        have_one = false;
+        // only leaves have objects
+        for (auto obj = objects.begin(); obj != objects.end(); obj ++) {
+            isect current;
+            BoundingBox bb = (*obj)->getBoundingBox();
+            if ((*obj)->intersect(r, current)) {
+                if (!have_one || current.t < i.t) {
+                    i = current;
+                    have_one = true;
+                }
+            }
+        }
+        if (objects.size() != 0) {
+            if (!have_one)
+              i.setT(1000.0);
+            return have_one;
+        }
+        // get intersection point
+        double min = r.at(tmin)[currentAxis];
+        double max = r.at(tmax)[currentAxis];
+        // determine the sequence of visiting nodes
+        if (min <= max && max - RAY_EPSILON <= split) { 
+          return left->intersect(r, i); 
+        }
+
+        if (min <= max && min + RAY_EPSILON >= split) { 
+          return right->intersect(r, i); 
+        }
+
+        if (min <= max && min - RAY_EPSILON <= split && split <= max + RAY_EPSILON) {
+            bool leftIntersect = left->intersect(r, i);
+            if (leftIntersect  && r.at(i.t)[currentAxis] <= split ) 
+              return true;
+            if (right->intersect(r, i)) 
+              return true;
+        } 
+
+        if (min > max && max + RAY_EPSILON >= split) { 
+          return right->intersect(r, i); 
+        }
+
+        if (min > max && min - RAY_EPSILON <= split) { 
+          return left->intersect(r, i); 
+        }
+
+        if (min > max && max -RAY_EPSILON <= split && split <= min + RAY_EPSILON) { 
+            bool rightIntersect = right->intersect(r, i);
+            if (rightIntersect  &&  r.at(i.t)[currentAxis] >= split) 
+                return true;
+            if (left->intersect(r, i)) 
+                return true;
+        }
+        return false;
+    }
+
+    //   bool intersect(ray& r, isect& i, int unused) {
+    //     std::stack<KdTree*> s;
+    //     s.push(this);
+    //     while (!s.empty()) {
+    //       KdTree *current = s.top();
+    //       s.pop();
+    //       double tmin = 0.0;
+    //       double tmax = 0.0;
+    //       bool have_one = current->getBoundary().intersect(r, tmin, tmax);
+          
+    //       if (!have_one) 
+    //         return false;
+    //       have_one = false;
+    //       if (current->getObjects().size() != 0) {
+    //           std::vector<Geometry*> objects = current->getObjects();
+    //           for (auto obj = objects.begin(); obj != objects.end(); obj ++) {
+    //               isect currentIn;
+    //               if ((*obj)->intersect(r, currentIn)) {
+    //                   if (!have_one || currentIn.t < i.t) {
+    //                       i = currentIn;
+    //                       have_one = true;
+    //                   }
+    //               }
+    //           }
+    //           return have_one;
+    //       }
+    //       if (left != nullptr)
+    //         s.push(left);
+    //       if (right != nullptr)
+    //         s.push(right);
+    //     }
+    //     return false;
+    // }
+
+};
+
+
+class Scene {
+
+public:
+  typedef std::vector<Light*>::iterator	liter;
+  typedef std::vector<Light*>::const_iterator cliter;
+  typedef std::vector<Geometry*>::iterator giter;
+  typedef std::vector<Geometry*>::const_iterator cgiter;
+
+  TransformRoot transformRoot;
+
+  Scene() : transformRoot(), objects(), lights() {}
+  virtual ~Scene();
+
+  void add( Geometry* obj ) {
+    obj->ComputeBoundingBox();
+	sceneBounds.merge(obj->getBoundingBox());
+    objects.push_back(obj);
+  }
+  void add(Light* light) { lights.push_back(light); }
+
+  bool intersect(ray& r, isect& i) const;
+
+  std::vector<Light*>::const_iterator beginLights() const { return lights.begin(); }
+  std::vector<Light*>::const_iterator endLights() const { return lights.end(); }
+
+  std::vector<Geometry*>::const_iterator beginObjects() const { return objects.begin(); }
+  std::vector<Geometry*>::const_iterator endObjects() const { return objects.end(); }
+        
+  const Camera& getCamera() const { return camera; }
+  Camera& getCamera() { return camera; }
+
+  // For efficiency reasons, we'll store texture maps in a cache
+  // in the Scene.  This makes sure they get deleted when the scene
+  // is destroyed.
+  TextureMap* getTexture( string name );
+
+  // These two functions are for handling ambient light; in the Phong model,
+  // the "ambient" light is considered a property of the _scene_ as a whole
+  // and hence should be set here.
+  Vec3d ambient() const	{ return ambientIntensity; }
+  void addAmbient( const Vec3d& ambient ) { ambientIntensity += ambient; }
+
+  void glDraw(int quality, bool actualMaterials, bool actualTextures) const;
+
+  const BoundingBox& bounds() const { return sceneBounds; }
+
+  void buildKdTree(int depth, int size){//cindy
+    giter g;
+    for( g = objects.begin(); g != objects.end(); ++g ){
+      (*g)->buildKdTree();
+    }
+
+    // for(auto obj: objects){
+    //   cout<<"enter1\n";
+    //   obj->buildKdTree();
+    // }
+    kdtree = new KdTree(depth, sceneBounds, size);
+    //cout<<"enter\n";
+    kdtree->addObjects(objects);
+    //cout<<"enter\n";
+
+  }
+
+ private:
+  std::vector<Geometry*> objects;
+  std::vector<Geometry*> nonboundedobjects;
+  std::vector<Geometry*> boundedobjects;
+  std::vector<Light*> lights;
+  Camera camera;
+
+  // This is the total amount of ambient light in the scene
+  // (used as the I_a in the Phong shading model)
+  Vec3d ambientIntensity;
+
+  typedef std::map< std::string, TextureMap* > tmap;
+  tmap textureCache;
+	
+  // Each object in the scene, provided that it has hasBoundingBoxCapability(),
+  // must fall within this bounding box.  Objects that don't have hasBoundingBoxCapability()
+  // are exempt from this requirement.
+  BoundingBox sceneBounds;
+  
+  //KdTree<Geometry>* kdtree;
+  KdTree* kdtree;//cindy
+
+
+ public:
+  // This is used for debugging purposes only.
+  mutable std::vector<std::pair<ray*, isect*> > intersectCache;
+};
+
+#endif // __SCENE_H__
